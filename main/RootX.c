d2206bec876bf7969859d79301e5148f9e106b8b
xiter00 <king@andyy.my.id>
2026-08-01 20:03:30 +0700

    ff

--- main/RootX.c
+++ main/RootX.c
@@ -374,8 +374,8 @@ void app_main(void) {
     vTaskDelay(pdMS_TO_TICKS(2000));
 
     // --- 3. HACK AUTO-CONNECT WIFI BUAT DEV ---
-    strcpy(connSSID, "AYYUBI"); 
-    strcpy(inputPassword, "rumahabi123");
+    strcpy(connSSID, "MASJID BAITUS SHOLIHIN"); 
+    strcpy(inputPassword, "ayo sholat");
     vTaskDelay(pdMS_TO_TICKS(100));
     triggerConnect = true; 
 
